111219ed0a00373c51f36e2fbc40583c4333b1b1
Ben Murray <ben@duosecurity.com>
2013-03-07 10:03:26 -0500

    Add extension, predelay, postdelay, and message arguments to verify.call

--- duo_client/verify.py
+++ duo_client/verify.py
@@ -9,15 +9,30 @@ import client
 SIG_VERSION = 1
 
 def call(ikey, skey, host, phone,
+         extension=None,
+         predelay=None,
+         postdelay=None,
+         message='The PIN is <pin>',
          ca=None):
     """
     Return a (PIN, txid) tuple from the response for a call API call.
     """
+    kwargs = {}
+    if ca is not None:
+        kwargs['ca'] = ca
+    if extension is not None:
+        kwargs['extension'] = extension
+    if predelay is not None:
+        kwargs['predelay'] = predelay
+    if postdelay is not None:
+        kwargs['postdelay'] = postdelay
     response = client.call_json_api(
         ikey, skey, host, 'POST', '/verify/v1/call.json',
-        ca=ca,
         sig_version=SIG_VERSION,
-        phone=phone, message='The PIN is <pin>')
+        phone=phone,
+        message=message,
+        **kwargs
+    )
     return (response['pin'], response['txid'])
 
 def status(ikey, skey, host, txid,
