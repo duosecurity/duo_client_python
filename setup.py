90b80d235a5c8963f09a4a88e68b38e406a07f63
Ben Murray <ben@duosecurity.com>
2013-03-26 17:12:11 -0400

    Fixes #3 Add distutils setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='duo_client',
+    version='2.0',
+    description='Reference client for Duo Security APIs',
+    author='Duo Security, Inc.',
+    author_email='support@duosecurity.com',
+    url='https://github.com/duosecurity/duo_client_python',
+    packages=['duo_client'],
+)
