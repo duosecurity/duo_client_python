e6622d40e6a64e3170d9cdd5ae6c5c4823f905ff
Jake Payton <jpayton@duosecurity.com>
2018-04-25 12:23:41 -0400

    Remove references to deprecated Verify API

--- duo_client/__init__.py
+++ duo_client/__init__.py
@@ -3,11 +3,9 @@ from .accounts import Accounts
 from .admin import Admin
 from .auth import Auth
 from .client import __version__
-from .verify import Verify
 
 __all__ = [
     'Accounts',
     'Admin',
     'Auth',
-    'Verify',
 ]
