e6fcf32259d913b0d13c68dfb5941fac4045341b
Chris Cassell <2981417+ccassell@users.noreply.github.com>
2018-11-15 09:51:25 -0500

    Add optional pagination to admin.get_user_bypass_codes

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -184,10 +184,50 @@ class TestAdmin(unittest.TestCase):
         self.assertEqual(util.params_to_dict(args),
                          {'account_id':[self.client.account_id]})
 
+    def test_get_user_bypass_codes_iterator(self):
+        """ Test to get bypass codes iterator by user id.
+        """
+        iterator = self.client_list.get_user_bypass_codes_iterator(
+            'DU012345678901234567')
+        response = next(iterator)
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(
+            uri,
+            '/admin/v1/users/DU012345678901234567/bypass_codes')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {
+                'account_id': [self.client.account_id],
+                'limit': ['100'],
+                'offset': ['0'],
+            })
+
     def test_get_user_bypass_codes(self):
         """ Test to get bypass codes by user id.
         """
-        response = self.client.get_user_bypass_codes('DU012345678901234567')
+        response = self.client_list.get_user_bypass_codes(
+            'DU012345678901234567')[0]
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(
+            uri,
+            '/admin/v1/users/DU012345678901234567/bypass_codes')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {
+                'account_id': [self.client.account_id],
+                'limit': ['100'],
+                'offset': ['0'],
+            })
+
+    def test_get_user_bypass_codes_with_offset(self):
+        """ Test to get bypass codes by user id with pagination params.
+        """
+        response = self.client_list.get_user_bypass_codes(
+            'DU012345678901234567', offset=30)[0]
         uri, args = response['uri'].split('?')
 
         self.assertEqual(response['method'], 'GET')
@@ -195,7 +235,47 @@ class TestAdmin(unittest.TestCase):
             uri,
             '/admin/v1/users/DU012345678901234567/bypass_codes')
         self.assertEqual(util.params_to_dict(args),
-                         {'account_id': [self.client.account_id]})
+                         {
+                            'account_id': [self.client.account_id],
+                            'limit': ['100'],
+                            'offset': ['0'],
+                         })
+
+    def test_get_user_bypass_codes_with_limit(self):
+        """ Test to get bypass codes by user id with pagination params.
+        """
+        response = self.client_list.get_user_bypass_codes(
+            'DU012345678901234567', limit=10)[0]
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(
+            uri,
+            '/admin/v1/users/DU012345678901234567/bypass_codes')
+        self.assertEqual(util.params_to_dict(args),
+                         {
+                            'account_id': [self.client.account_id],
+                            'limit': ['10'],
+                            'offset': ['0'],
+                         })
+
+    def test_get_user_bypass_codes_with_limit_and_offset(self):
+        """ Test to get bypass codes by user id with pagination params.
+        """
+        response = self.client_list.get_user_bypass_codes(
+            'DU012345678901234567', limit=10, offset=30)[0]
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(
+            uri,
+            '/admin/v1/users/DU012345678901234567/bypass_codes')
+        self.assertEqual(util.params_to_dict(args),
+                         {
+                            'account_id': [self.client.account_id],
+                            'limit': ['10'],
+                            'offset': ['30'],
+                         })
 
     def test_get_bypass_codes(self):
         """ Test to get bypass codes.
