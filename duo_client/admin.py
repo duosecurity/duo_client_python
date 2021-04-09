692727260148f76f8a69b09ab426c568c57f2adf
Josh Cheng <jcheng@duosecurity.com>
2021-04-09 11:34:29 -0400

    Add admin client methods for external password management API endpoints

--- duo_client/admin.py
+++ duo_client/admin.py
@@ -2523,14 +2523,13 @@ class Admin(client.Client):
         response = self.json_api_call('GET', path, {})
         return response
 
-    def add_admin(self, name, email, phone, password, role=None):
+    def add_admin(self, name, email, phone, role=None):
         """
         Create an administrator and adds it to a customer.
 
         name - <str:the name of the administrator>
         email - <str:email address>
         phone - <str:phone number>
-        password - <str:password>
         role - <str|None:role>
 
         Returns the added administrator.  See the adminapi docs.
@@ -2544,8 +2543,6 @@ class Admin(client.Client):
             params['email'] = email
         if phone is not None:
             params['phone'] = phone
-        if password is not None:
-            params['password'] = password
         if role is not None:
             params['role'] = role
         response = self.json_api_call('POST', '/admin/v1/admins', params)
@@ -2554,7 +2551,6 @@ class Admin(client.Client):
     def update_admin(self, admin_id,
                      name=None,
                      phone=None,
-                     password=None,
                      password_change_required=None,
                      ):
         """
@@ -2577,8 +2573,6 @@ class Admin(client.Client):
             params['name'] = name
         if phone is not None:
             params['phone'] = phone
-        if password is not None:
-            params['password'] = password
         if password_change_required is not None:
             params['password_change_required'] = password_change_required
         response = self.json_api_call('POST', path, params)
@@ -2651,6 +2645,80 @@ class Admin(client.Client):
         response = self.json_api_call('POST',
                                       '/admin/v1/admins/activations',
                                       params)
+        return response
+
+    def get_external_password_mgmt_statuses(self, limit=None, offset=0):
+        """
+        Returns a paged list of administrators indicating whether they
+        have been configured for external password management.
+        Args:
+            limit: The max number of admins to fetch at once. Default None
+            offset: If a limit is passed, the offset to start retrieval.
+                    Default 0
+
+        Returns a list of administrators' external password management
+            statuses.  See the adminapi docs.
+
+        Raises RuntimeError on error.
+        """
+
+        (limit, offset) = self.normalize_paging_args(limit, offset)
+        if limit:
+            return self.json_api_call(
+                'GET',
+                '/admin/v1/admins/password_mgmt',
+                {'limit': limit, 'offset': offset}
+            )
+
+        iterator = self.json_paging_api_call(
+            'GET', '/admin/v1/admins/password_mgmt', {})
+
+        return list(iterator)
+
+    def get_external_password_mgmt_status_for_admin(self, admin_id):
+        """
+        Returns the external password management status for an admin
+
+        admin_id - The id of the admin.
+
+        Returns an external password management status. See the
+        adminapi docs.
+
+        Raises RuntimeError on error.
+        """
+
+        admin_id = six.moves.urllib.parse.quote_plus(str(admin_id))
+        path = '/admin/v1/admins/{}/password_mgmt'.format(admin_id)
+        response = self.json_api_call('GET', path, {})
+        return response
+
+    def update_admin_password_mgmt_status(
+        self, admin_id, has_external_password_mgmt=None,
+        password=None):
+        """
+        Enable or disable an admin for external password management,
+        and optionally set the password for an admin
+
+        admin_id - The id of the admin.
+        has_external_password_mgmt - whether or not this admin's password
+            is managed via API.
+        password - New password for the admin.
+
+        Returns an external password management status. See the
+        adminapi docs.
+
+        Raises RuntimeError on error.
+        """
+
+        params = {}
+        if password is not None:
+            params['password'] = password
+        if has_external_password_mgmt is not None:
+            params['has_external_password_mgmt'] = str(has_external_password_mgmt)
+
+        admin_id = six.moves.urllib.parse.quote_plus(str(admin_id))
+        path = '/admin/v1/admins/{}/password_mgmt'.format(admin_id)
+        response = self.json_api_call('POST', path, params)
         return response
 
     def get_logo(self):
--- tests/admin/test_admins.py
+++ tests/admin/test_admins.py
@@ -74,3 +74,103 @@ class TestAdmins(TestAdmin):
                 'limit': ['100'],
                 'offset': ['0'],
             })
+
+    def test_get_external_password_mgmt_statuses(self):
+
+        response = self.client_list.get_external_password_mgmt_statuses()
+        response = response[0]
+        self.assertEqual(response['method'], 'GET')
+        (uri, args) = response['uri'].split('?')
+        self.assertEqual(uri, '/admin/v1/admins/password_mgmt')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {
+                'account_id': [self.client.account_id],
+                'limit': ['100'],
+                'offset': ['0'],
+            })
+
+    def test_get_external_password_mgmt_statuses_with_limit(self):
+        response = self.client_list.get_external_password_mgmt_statuses(
+            limit='20')
+        response = response[0]
+        self.assertEqual(response['method'], 'GET')
+        (uri, args) = response['uri'].split('?')
+        self.assertEqual(uri, '/admin/v1/admins/password_mgmt')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {
+                'account_id': [self.client.account_id],
+                'limit': ['20'],
+                'offset': ['0'],
+            })
+
+    def test_get_external_password_mgmt_statusesg_with_limit_offset(self):
+        response = self.client_list.get_external_password_mgmt_statuses(
+            limit='20', offset='2')
+        response = response[0]
+        self.assertEqual(response['method'], 'GET')
+        (uri, args) = response['uri'].split('?')
+        self.assertEqual(uri, '/admin/v1/admins/password_mgmt')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {
+                'account_id': [self.client.account_id],
+                'limit': ['20'],
+                'offset': ['2'],
+            })
+
+    def test_get_external_password_mgmt_statuses_with_offset(self):
+        response = self.client_list.get_external_password_mgmt_statuses(
+            offset=9001)
+        response = response[0]
+        self.assertEqual(response['method'], 'GET')
+        (uri, args) = response['uri'].split('?')
+        self.assertEqual(uri, '/admin/v1/admins/password_mgmt')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {
+                'account_id': [self.client.account_id],
+                'limit': ['100'],
+                'offset': ['0'],
+            })
+
+    def test_get_external_password_mgmt_status_for_admin(self):
+        response = self.client_list.get_external_password_mgmt_status_for_admin(
+            'DFAKEADMINID')
+        response = response[0]
+        self.assertEqual(response['method'], 'GET')
+        (uri, args) = response['uri'].split('?')
+        self.assertEqual(uri, '/admin/v1/admins/DFAKEADMINID/password_mgmt')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {
+                'account_id': [self.client.account_id],
+            })
+
+    def test_update_admin_password_mgmt_status(self):
+        response = self.client_list.update_admin_password_mgmt_status(
+            'DFAKEADMINID', has_external_password_mgmt='False')
+        response = response[0]
+        self.assertEqual(response['method'], 'POST')
+        self.assertEqual(response['uri'], '/admin/v1/admins/DFAKEADMINID/password_mgmt')
+        self.assertEqual(
+            util.params_to_dict(response['body']),
+            {
+                'account_id': [self.client.account_id],
+                'has_external_password_mgmt': ['False']
+            })
+
+    def test_update_admin_password_mgmt_status_set_password(self):
+        response = self.client_list.update_admin_password_mgmt_status(
+            'DFAKEADMINID', has_external_password_mgmt='True', password='dolphins')
+        response = response[0]
+        self.assertEqual(response['method'], 'POST')
+        self.assertEqual(response['uri'], '/admin/v1/admins/DFAKEADMINID/password_mgmt')
+        self.assertEqual(
+            util.params_to_dict(response['body']),
+            {
+                'account_id': [self.client.account_id],
+                'has_external_password_mgmt': ['True'],
+                'password': ['dolphins']
+            })
