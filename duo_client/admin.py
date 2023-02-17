709cae896b66d2dd664b0812c0b65fe1cddcb00d
Ben Carsten <bcarsten@duosecurity.com>
2023-02-17 10:24:43 -0600

    lookup and use child api hostname if available when making AccountAdmin calls

--- duo_client/admin.py
+++ duo_client/admin.py
@@ -173,7 +173,7 @@ from __future__ import absolute_import
 
 import six.moves.urllib
 
-from . import client
+from . import client, Accounts
 import six
 import warnings
 import time
@@ -3282,6 +3282,17 @@ class AccountAdmin(Admin):
         """Initializes an AccountAdmin for administering a child account.
            account_id is the account id of the child account.
            See the Client base class for other parameters."""
+        child_api_host =  Accounts.child_map.get(account_id, None)
+        if child_api_host is None:
+            child_api_host = kwargs.get('host')
+            try:
+                accounts_api = Accounts(**kwargs)
+                accounts_api.get_child_accounts()
+                child_api_host =  Accounts.child_map.get(account_id, kwargs['host'])
+            except RuntimeError:
+                pass
+        kwargs['host'] = child_api_host
+        
         super(AccountAdmin, self).__init__(**kwargs)
         self.account_id = account_id
 
