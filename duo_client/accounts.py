cdf7da6e9230fbf8e92a26f488ccfe3406bbd9e2
jincopunk <jincopunk@users.noreply.github.com>
2023-10-09 08:44:13 -0500

    lookup and use child api hostname if available when making AccountAdmin calls (#194)
    Co-authored-by: Ben Carsten <bcarsten@duosecurity.com>
    Co-authored-by: Aaron McConnell <amcconnell@duosecurity.com>

--- duo_client/accounts.py
+++ duo_client/accounts.py
@@ -7,6 +7,8 @@ from __future__ import absolute_import
 from . import client
 
 class Accounts(client.Client):
+    child_map = {}
+
     def get_child_accounts(self):
         """
         Return a list of all child accounts of the integration's account.
@@ -15,6 +17,12 @@ class Accounts(client.Client):
         response = self.json_api_call('POST',
                                       '/accounts/v1/account/list',
                                       params)
+        if response and isinstance(response, list):
+            for account in response:
+                account_id = account.get('account_id', None)
+                api_hostname = account.get('api_hostname', None)
+                if account_id and api_hostname:
+                    Accounts.child_map[account_id] = api_hostname
         return response
 
     def create_account(self, name):
