bbc41b329850f391358265b2b6df3eea4de16d18
Mark Tripod <mtripod@duosecurity.com>
2025-03-19 12:45:29 -0400

    feat: add support for managing blocked devices in admin API
    Added methods to block, unblock, and fetch blocked devices via the admin API. Updated test cases to validate the new functionality, including support for pagination and operations by device ID.

--- tests/admin/test_registered_devices.py
+++ tests/admin/test_registered_devices.py
@@ -1,10 +1,12 @@
+import json
+
 from .base import TestAdmin
 from .. import util
 
 
 class TestRegisteredDevices(TestAdmin):
     def test_get_registered_devices_generator(self):
-        """ Test to get desktop tokens generator.
+        """ Test to get registered devices generator.
         """
         generator = self.client_list.get_registered_devices_generator()
         response = next(generator)
@@ -16,7 +18,7 @@ class TestRegisteredDevices(TestAdmin):
                 {'account_id': [self.client_list.account_id], 'limit': ['100'], 'offset': ['0'], })
 
     def test_get_registered_devices(self):
-        """ Test to get desktop tokens without params.
+        """ Test to get registered devices without params.
         """
         response = self.client_list.get_registered_devices()[0]
         uri, args = response['uri'].split('?')
@@ -27,7 +29,7 @@ class TestRegisteredDevices(TestAdmin):
                 {'account_id': [self.client_list.account_id], 'limit': ['100'], 'offset': ['0'], })
 
     def test_get_registered_devices_limit(self):
-        """ Test to get desktop tokens with limit.
+        """ Test to get registered devices with limit.
         """
         response = self.client_list.get_registered_devices(limit='20')[0]
         uri, args = response['uri'].split('?')
@@ -38,7 +40,7 @@ class TestRegisteredDevices(TestAdmin):
                 {'account_id': [self.client_list.account_id], 'limit': ['20'], 'offset': ['0'], })
 
     def test_get_registered_devices_offset(self):
-        """ Test to get desktop tokens with offset.
+        """ Test to get registered devices with offset.
         """
         response = self.client_list.get_registered_devices(offset='20')[0]
         uri, args = response['uri'].split('?')
@@ -49,7 +51,7 @@ class TestRegisteredDevices(TestAdmin):
                 {'account_id': [self.client_list.account_id], 'limit': ['100'], 'offset': ['0'], })
 
     def test_get_registered_devices_limit_offset(self):
-        """ Test to get desktop tokens with limit and offset.
+        """ Test to get registered devices with limit and offset.
         """
         response = self.client_list.get_registered_devices(limit='20', offset='2')[0]
         uri, args = response['uri'].split('?')
@@ -68,3 +70,107 @@ class TestRegisteredDevices(TestAdmin):
         self.assertEqual(response['method'], 'DELETE')
         self.assertEqual(uri, '/admin/v1/registered_devices/CRSFWW1YWVNUXMBJ1J29')
         self.assertEqual(util.params_to_dict(args), {'account_id': [self.client.account_id]})
+
+    def test_get_blocked_devices_generator(self):
+        """ Test to get blocked devices generator.
+        """
+        generator = self.client_list.get_blocked_devices_generator()
+        response = next(generator)
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(uri, '/admin/v1/registered_devices/blocked')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {'account_id': [self.client_list.account_id], 'limit': ['100'], 'offset': ['0'], }
+            )
+
+    def test_get_blocked_devices(self):
+        """ Test to get blocked devices without params.
+        """
+        response = self.client_list.get_blocked_devices()[0]
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(uri, '/admin/v1/registered_devices/blocked')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {'account_id': [self.client_list.account_id], 'limit': ['100'], 'offset': ['0'], }
+            )
+
+    def test_get_blocked_devices_limit(self):
+        """ Test to get blocked devices with limit.
+        """
+        response = self.client_list.get_blocked_devices(limit='20')[0]
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(uri, '/admin/v1/registered_devices/blocked')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {'account_id': [self.client_list.account_id], 'limit': ['20'], 'offset': ['0'], }
+            )
+
+    def test_get_blocked_devices_offset(self):
+        """ Test to get blocked devices with offset.
+        """
+        response = self.client_list.get_blocked_devices(offset='20')[0]
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(uri, '/admin/v1/registered_devices/blocked')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {'account_id': [self.client_list.account_id], 'limit': ['100'], 'offset': ['0'], }
+            )
+
+    def test_get_blocked_devices_limit_offset(self):
+        """ Test to get blocked devices with limit and offset.
+        """
+        response = self.client_list.get_blocked_devices(limit='20', offset='2')[0]
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'GET')
+        self.assertEqual(uri, '/admin/v1/registered_devices/blocked')
+        self.assertEqual(
+            util.params_to_dict(args),
+            {'account_id': [self.client_list.account_id], 'limit': ['20'], 'offset': ['2'], }
+            )
+
+    def test_block_registered_device_by_id(self):
+        """ Test to block registered device by registered device id.
+        """
+        response = self.client.block_registered_device_by_id('CRSFWW1YWVNUXMBJ1J29')
+
+        self.assertEqual(response['method'], 'POST')
+        self.assertEqual(response['uri'], '/admin/v1/registered_devices/blocked/CRSFWW1YWVNUXMBJ1J29')
+        self.assertEqual(json.loads(response['body'])['account_id'], self.client.account_id)
+
+    def test_block_registered_devices(self):
+        """ Test to block registered devices.
+        """
+        response = self.client.block_registered_devices(['CRSFWW1YWVNUXMBJ1J29', 'CRSFWW1YWVNUXMBJ1J30'])
+
+        self.assertEqual(response['method'], 'POST')
+        self.assertEqual(response['uri'], '/admin/v1/registered_devices/blocked')
+        self.assertEqual(json.loads(response['body'])['account_id'], self.client.account_id)
+
+    def test_unblock_registered_device_by_id(self):
+        """ Test to unblock registered device by registered device id.
+        """
+        response = self.client.unblock_registered_device_by_id('CRSFWW1YWVNUXMBJ1J29')
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'DELETE')
+        self.assertEqual(uri, '/admin/v1/registered_devices/blocked/CRSFWW1YWVNUXMBJ1J29')
+        self.assertEqual(util.params_to_dict(args), {'account_id': [self.client.account_id]})
+
+    def test_unblock_registered_devices(self):
+        """ Test to unblock registered devices.
+        """
+        response = self.client.unblock_registered_devices(['CRSFWW1YWVNUXMBJ1J29', 'CRSFWW1YWVNUXMBJ1J30'])
+        uri, args = response['uri'].split('?')
+
+        self.assertEqual(response['method'], 'DELETE')
+        self.assertEqual(uri, '/admin/v1/registered_devices/blocked')
+        self.assertEqual(util.params_to_dict(args)['account_id'], [self.client.account_id])
